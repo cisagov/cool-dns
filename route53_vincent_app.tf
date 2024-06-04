f84778be29d2bb65960ea72b97c7fee1cfdbd4ff
Jeremy Morrow <jeremy.morrow@cisa.dhs.gov>
2024-06-04 15:34:52 -0400

    Create dev and staging DNS resources for VINCE-NT application

---
+++ route53_vincent_app.tf
@@ -0,0 +1,33 @@
+# ------------------------------------------------------------------------------
+# Resource records that support VINCE-NT (Vulnerability Information and
+# Coordination Environment - New Technology)
+
+# ------------------------------------------------------------------------------
+# Development entries
+# ------------------------------------------------------------------------------
+
+resource "aws_route53_record" "vincent_dev_CNAME" {
+  provider = aws.route53resourcechange
+
+  name    = "dev.vincent.${aws_route53_zone.cyber_dhs_gov.name}"
+  records = ["vincent-dev-13498182.us-gov-west-1.elb.amazonaws.com"]
+  ttl     = 300
+  type    = "CNAME"
+  zone_id = aws_route53_zone.cyber_dhs_gov.zone_id
+}
+
+# ------------------------------------------------------------------------------
+# Staging entries
+# ------------------------------------------------------------------------------
+
+resource "aws_route53_record" "vincent_stage_CNAME" {
+  provider = aws.route53resourcechange
+
+  name    = "vincent.rsaa.${aws_route53_zone.cyber_dhs_gov.name}"
+  records = ["vincent-stage-1522298475.us-gov-west-1.elb.amazonaws.com"]
+  ttl     = 300
+  type    = "CNAME"
+  zone_id = aws_route53_zone.cyber_dhs_gov.zone_id
+}
+
+
